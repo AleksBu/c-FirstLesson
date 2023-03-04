ee6f4ab2598d6523fe0f843b2c28d7cb0349b9f1
AleksBu <yakut0385@gmail.com>
2023-03-04 12:23:27 +0300

    method

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор Алексей");
+}
+
+Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg:"Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    } 
+}
+//Method21("Текст", 4);
+Method21(count: 4, msg: "новый Текст");
