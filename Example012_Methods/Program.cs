363d5392a1e92adb8a7a66c9307844ad785926e5
AleksBu <yakut0385@gmail.com>
2023-03-04 12:42:15 +0300

    method2

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -23,4 +23,32 @@ void Method21(string msg, int count)
     } 
 }
 //Method21("Текст", 4);
-Method21(count: 4, msg: "новый Текст");
+//Method21(count: 4, msg: "новый Текст");
+
+// Вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int Year = Method3();
+Console.WriteLine(Year);
+
+// Вид 4
+
+string Method4(int count, string text)
+
+{
+    int i = 0;
+    string result = String.Empty;
+
+     while (i < count)
+     {
+        result = result + text;
+        i++;
+     }
+     return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
