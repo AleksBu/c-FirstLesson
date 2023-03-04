c7f49b97e915c838ba12f7360d56fe4240f18b8d
AleksBu <yakut0385@gmail.com>
2023-03-04 13:16:41 +0300

    Method

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -13,15 +13,15 @@ void Method2(string msg)
 }
 Method2(msg:"Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    } 
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     } 
+// }
 //Method21("Текст", 4);
 //Method21(count: 4, msg: "новый Текст");
 
@@ -36,19 +36,71 @@ Console.WriteLine(Year);
 
 // Вид 4
 
+// string Method4(int count, string text)
+
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//      while (i < count)
+//      {
+//         result = result + text;
+//         i++;
+//      }
+//      return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 
 {
-    int i = 0;
-    string result = String.Empty;
 
-     while (i < count)
+    string result = String.Empty;
+    for( int i = 0; i < count; i++)
+
      {
         result = result + text;
-        i++;
      }
      return result;
 }
 
 string res = Method4(10, "z");
 Console.WriteLine(res);
+
+
+for( int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+string text = "Я помню чудное мгновенье: "
+            + "Передо мной явилась ты, "
+            + "Как мимолётное виденье, "
+            + "Как гений чистой красоты.";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
